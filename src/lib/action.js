fe9c0fff873f3503c6a11e27df83394461f301db
alphardhafiz <bucialphard@gmail.com>
2024-04-21 17:46:00 +0700

    Create Register UI and API register

--- src/lib/action.js
+++ src/lib/action.js
@@ -1,9 +1,10 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { Post } from "./models";
+import { Post, User } from "./models";
 import { connectToDb } from "./utils";
 import { signIn, signOut } from "./auth";
+import bcrypt from 'bcrypt'
 
 export const addPost = async (formData) => {
   //   const title = formData.get("title");
@@ -43,14 +44,49 @@ export const deletePost = async (formData) => {
   }
 };
 
-export   const handleGithubLogin = async () => {
+export const handleGithubLogin = async () => {
   "use server";
 
   await signIn("github");
 };
 
 export const handleLogout = async () => {
-  "use server"
+  "use server";
 
   await signOut();
-}
+};
+
+export const register = async (formData) => {
+  const { username, email, password, img, passwordRepeat } =
+    Object.fromEntries(formData);
+
+  if (password !== passwordRepeat) {
+    return "Password do not match";
+  }
+
+  try {
+    connectToDb();
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return "Username already exists";
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      img,
+    });
+
+    await newUser.save();
+    console.log("saved to db");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
+};
